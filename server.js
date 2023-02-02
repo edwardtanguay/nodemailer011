88acee5bc28365048410b4193f89ffc790adc859
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-02 01:07:30 +0100

    finished

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ dotenv.config();
 var transporter = createTransport({
 	service: 'gmail',
 	auth: {
-		user: process.env.GOOGLE_MAIL_ACCOUNT_USER,
+		user: process.env.GOOGLE_MAIL_ACCOUNT_USER, // e.g. sitemailer92833
 		pass: process.env.GOOGLE_MAIL_NODEMAILER_PASSWORD, // this is the "app password" that you set up, not the main password for this mail account
 	},
 });
 
 var mailOptions = {
-	from: 'Language Community Site <edwardappmailer234@gmail.com>',
-	to: 'edwardtanguay@gmail.com',
+	from: `Language Community Site <${process.env.GOOGLE_MAIL_ACCOUNT_USER}@gmail.com>`,
+	to: 'thereceipient@domain.com', // change this to email you want to received test mails at
 	subject: 'Please confirm your registration',
 	html: `
 <h1>Please confirm your registration</h1>	
